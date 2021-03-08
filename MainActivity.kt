2dad0f41bce7d8685c8c528031a5d6ffbd116adf
jaiminipatel831 <73722649+jaiminipatel831@users.noreply.github.com>
2021-03-08 00:34:16 -0600

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.fragmentactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, firstFragment)
+            commit()
+
+        }
+        btnFragment1.setOnClickListner{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, firstFragment)
+                commit()
+
+            }
+        }
+        btnFragment2.setOnClickListner{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, secondFragment)
+                commit()
+
+            }
+        }
+
+
+    }
+
+}
